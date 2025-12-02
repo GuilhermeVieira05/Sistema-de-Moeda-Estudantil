59a6246f34098ba07d777eca2cf99e9fa2d4dcc9
Diogo C Brunoro <diogocaribebrunoro@gmail.com>
2025-12-01 22:52:59 -0300

    é natal

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,            // recomendado
+  eslint: {
+    ignoreDuringBuilds: true,       // ignora erros de lint durante build
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',             // permite imagens externas
+      },
+    ],
+  },
 };
 
 export default nextConfig;
